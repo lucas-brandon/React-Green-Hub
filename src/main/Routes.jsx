619c6c4a1ad8bc2009fd042fce525874f758620e
aluno <elly.estudos@gmail.com>
2020-10-21 18:12:26 -0300

    categorias

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Router, Route, Redirect, hashHistory } from 'react-router';
 
 import Home from '../pages/home/Index';
+import Categorias from '../pages/categorias/Index';
 //import About from '../about/About';
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/home' component={Home}></Route>
+        <Route path='/Categorias' component={Categorias}></Route>
         <Redirect from='*' to='/home'></Redirect>
     </Router>
 )
