b3eee515c4423c397b1071c71337a6eaefa716b4
Mateus Farias <mateus.pistillo@hotmail.com>
2020-10-22 11:16:43 -0300

    migracao da tela de perfil

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -4,6 +4,7 @@ import { Router, Route, Redirect, hashHistory } from 'react-router';
 import Home from '../pages/home/Index';
 import Register from '../pages/register/register';
 import DetalhesProduto from '../pages/detalhesProduto/detalhesProduto';
+import Profile from '../pages/profile/profile';
 //import About from '../about/About';
 
 export default props => (
@@ -11,6 +12,7 @@ export default props => (
         <Route path='/home' component={Home}></Route>
         <Route path='/register' component={Register}></Route>
         <Route path='/detalhesProduto' component={DetalhesProduto}></Route>
+        <Route path='/profile' component={Profile}></Route>
         <Redirect from='*' to='/home'></Redirect>
     </Router>
 )
